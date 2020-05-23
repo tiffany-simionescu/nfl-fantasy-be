9a3718b23136c42be1047c03ed7c6025d45a62be
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-05-23 13:33:51 -0400

    Updated player-router for codeclimate

--- routers/player-router.js
+++ routers/player-router.js
@@ -51,12 +51,10 @@ playerRouter.get('/:id', validatePlayerId(), (req, res) => {
 playerRouter.get('/fan/:id', validateFanId(), (req, res) => {
   Players.findFan(req.params.id)
     .then(fanPlayers => {
-      res.status(200).json(fanPlayers);
+      res.json(fanPlayers);
     })
     .catch(err => {
-      res.status(500).json({
-        message: "There was an error trying to retrieve the fan's players. Please try again later."
-      })
+      res.json(err)
     })
 })
 
