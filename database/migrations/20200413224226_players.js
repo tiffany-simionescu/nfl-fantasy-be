ab10a61e5cd952a7e1d60d7a47ffdcbec2c21920
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-05-27 00:29:23 -0400

    updated fan migration and seed. Still needs update

--- database/migrations/20200413224226_players.js
+++ database/migrations/20200413224226_players.js
@@ -3,49 +3,71 @@ exports.up = async function (knex) {
     players.increments();
     players.string("playerID");
 
-    players.string("playerFirst", 128).notNullable();
+    players.string("playerFirst", 128)
+      .notNullable();
 
-    players.string("playerLast", 128).notNullable();
+    players.string("playerLast", 128)
+      .notNullable();
 
-    players.string("playerFull", 128).notNullable();
+    players.string("playerFull", 128)
+      .notNullable();
 
-    players.string("position").notNullable();
+    players.string("position")
+      .notNullable();
 
-    players.integer("startYear").notNullable();
+    players.integer("startYear")
+      .notNullable();
 
-    players.string("currentTeam", 128).notNullable();
+    players.string("currentTeam", 128)
+      .notNullable();
 
-    players.integer("height").notNullable();
+    players.integer("height")
+      .notNullable();
 
-    players.integer("weight").notNullable();
+    players.integer("weight")
+      .notNullable();
 
-    players.date("dob").notNullable();
+    players.date("dob")
+      .notNullable();
 
-    players.decimal("forty", 8, 2).notNullable();
+    players.decimal("forty", 8, 2)
+      .notNullable();
 
-    players.integer("bench").notNullable();
+    players.integer("bench")
+      .notNullable();
 
-    players.decimal("vertical", 8, 2).notNullable();
+    players.decimal("vertical", 8, 2)
+      .notNullable();
 
-    players.integer("broad").notNullable();
+    players.integer("broad")
+      .notNullable();
 
-    players.decimal("shuttle", 8, 2).notNullable();
+    players.decimal("shuttle", 8, 2)
+      .notNullable();
 
-    players.decimal("cone", 8, 2).notNullable();
+    players.decimal("cone", 8, 2)
+      .notNullable();
 
-    players.decimal("arm", 8, 2).notNullable();
+    players.decimal("arm", 8, 2)
+      .notNullable();
 
-    players.decimal("hand", 8, 2).notNullable();
+    players.decimal("hand", 8, 2)
+      .notNullable();
 
-    players.decimal("dpos", 8, 2).notNullable();
+    players.decimal("dpos", 8, 2)
+      .notNullable();
 
-    players.string("col", 128).notNullable();
+    players.string("col", 128)
+      .notNullable();
 
-    players.string("dv", 128).notNullable();
+    players.string("dv", 128)
+      .notNullable();
 
-    players.integer("jnum").notNullable();
+    players.integer("jnum")
+      .notNullable();
 
-    players.integer("dcp").notNullable();
+    players.integer("dcp")
+      .notNullable();
   });
 };
 
