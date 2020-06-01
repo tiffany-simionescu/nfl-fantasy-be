e344e19212eccc4b61b3c5a059b65da33c3ffc5a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-06-01 01:59:32 -0400

    codeclimate update

--- routers/fan-router.js
+++ routers/fan-router.js
@@ -27,7 +27,7 @@ function generateToken(fan) {
 fanRouter.get('/', (req, res) => {
   Fans.find()
     .then(fans => {
-    res.json(fans)
+    res.json(fans) 
   })
   .catch(err => {
     res.status(500).json({
